f2d3e6755054fd977481e0c6b07e76a070104164
yjt1212 <49463417+yjt1212@users.noreply.github.com>
2019-07-12 17:23:54 +0800

    Update node_inx.js

--- node_inx.js
+++ node_inx.js
@@ -4,20 +4,15 @@
  * 备注： 中间件
  */
 
-// 引入 express 模块
 var express = require('express');
 var app = express();
 
-// 引入 MongoDB 模块
-// 获得它的客户端对象：MongoClient
-// 用来连接 MongoDB
-var MongoClient = require('mongodb').MongoClient;
 
-// 获得连接 MongoDB 的字符串
-// 变量名为： DB_CONN_STR, 约定俗成的，不能随便乱写
+// 连接 MongoDB
+var MongoClient = require('mongodb').MongoClient;
 var DB_CONN_STR = 'mongodb://127.0.0.1:27017';
 
- // 解决跨域问题
+ // 解决跨域
 app.all('*', function(req, res, next) {
  	res.header("Access-Control-Allow-Origin", "*");
  	res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -39,10 +34,7 @@ app.get('/search_data', function(req, res){
 
 	// 连接数据库
 	MongoClient.connect(DB_CONN_STR, function(err, db){
-		// 数据库名： abcxx
 		var _dbo = db.db('abcxx');
-
-		// 集合名： first
 		var _collection = _dbo.collection('first');
 
 		_collection.find().toArray(function(err, result){
@@ -55,8 +47,7 @@ app.get('/search_data', function(req, res){
 	});
 });
 
-// 插入数据（增）
-// 如果没有返回值，就没有.send()方法
+// 插入数据
 app.get('/insert_data', function(req, res){
 
 	// 得到从vue中提交的内容
@@ -66,10 +57,7 @@ app.get('/insert_data', function(req, res){
 	// 连接数据库
 	MongoClient.connect(DB_CONN_STR, function(err, db){
 
-		// 数据库名： abcxx
 		var _dbo = db.db('abcxx');
-
-		// 集合名： first
 		var _collection = _dbo.collection('first');
 
 		_collection.insertOne(_msgObj, function(err, result){
@@ -83,7 +71,7 @@ app.get('/insert_data', function(req, res){
 	});
 });
 
-// 删除数据（删）
+// 删除数据
 app.get('/del_data', function(req, res){
 
 	// 得到从vue中提交的内容
@@ -92,21 +80,14 @@ app.get('/del_data', function(req, res){
 	// 这里需要使用_id的字符串的内容
 	var _idString = req.query._id;
 
-	// ps：生成一个 MongoDB 的 ID 对象
-	// mongoDB 中的 _id,不是字符串，是MongoDB的一个id对象
-	// 所以需要手动生成
+	// 生成一个 MongoDB 的 ID 对象
 	var _ObjectID = require('mongodb').ObjectID;
-
-	// _ObjId, 就是 mongoDB 的 id 对象
 	var _ObjId = _ObjectID.createFromHexString(_idString);
 
 	// 连接数据库
 	MongoClient.connect(DB_CONN_STR, function(err, db){
 
-		// 数据库名： abcxx
 		var _dbo = db.db('abcxx');
-
-		// 集合名： first
 		var _collection = _dbo.collection('first');
 
 		_collection.remove({"_id":_ObjId}, function(err, result){
@@ -129,10 +110,7 @@ app.get('/find_data', function(req, res){
 	// 连接数据库
 	MongoClient.connect(DB_CONN_STR, function(err, db){
 
-		// 数据库名： abcxx
 		var _dbo = db.db('abcxx');
-
-		// 集合名： first
 		var _collection = _dbo.collection('first');
 
 		_collection.findOne({"txt": _txt}, function(err, result){
@@ -150,17 +128,14 @@ app.get('/find_id', function(req, res){
 	var _idString = req.query._id;
 	// console.log( _idString );
 
-	// ps：生成一个 MongoDB 的 ID 对象
+	// 生成一个 MongoDB 的 ID 对象
 	var _ObjectID = require('mongodb').ObjectID;
 	var _findObjId = _ObjectID.createFromHexString(_idString);
 
 	// 连接数据库
 	MongoClient.connect(DB_CONN_STR, function(err, db){
 
-		// 数据库名： abcxx
 		var _dbo = db.db('abcxx');
-
-		// 集合名： first
 		var _collection = _dbo.collection('first');
 
 		_collection.findOne({"_id": _findObjId}, function(err, result){
@@ -180,17 +155,14 @@ app.get('/save_id', function(req, res){
 	// console.log( _saveObj );
 	var _id = _saveObj._id;
 
-	// ps：生成一个 MongoDB 的 ID 对象
+	// 生成一个 MongoDB 的 ID 对象
 	var _ObjectID = require('mongodb').ObjectID;
 	var _saveObjId = _ObjectID.createFromHexString(_id);
 
 	// 连接数据库
 	MongoClient.connect(DB_CONN_STR, function(err, db){
 
-		// 数据库名： abcxx
 		var _dbo = db.db('abcxx');
-
-		// 集合名： first
 		var _collection = _dbo.collection('first');
 
 		_collection.save({
